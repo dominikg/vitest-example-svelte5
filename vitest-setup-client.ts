7697287a68629953fea497a139f547bae4400d49
dominikg <dominik.goepel@gmx.de>
2024-12-09 19:43:44 +0100

    update all deps, fix issue introduced by MediaQuery addition in svelte 5.9

--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -2,3 +2,19 @@ import '@testing-library/jest-dom/vitest'
 import {vi} from 'vitest';
 
 // add global mocks here, eg for sveltekit '$app/stores' ?
+
+// needed for svelte/motion that exports new MediaQuery which calls window.matchMedia eagerly
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    enumerable: true,
+    value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
