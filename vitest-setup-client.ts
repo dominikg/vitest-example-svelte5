96002a2e330bbe78617e09ea6b9deff74263ac0a
dominikg <dominik.goepel@gmx.de>
2025-02-03 22:26:37 +0100

    bump versions again and add mock for window.matchMedia. move vitest.workspace.ts into vite.config.ts

--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -1,4 +1,20 @@
 import '@testing-library/jest-dom/vitest'
 import {vi} from 'vitest';
 
-// add global mocks here, eg for sveltekit '$app/stores' ?
+// required for svelte5 + jsdom as jsdom does not support matchMedia
+if(typeof window != undefined && !window.matchMedia) {
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        enumerable: true,
+        value: vi.fn().mockImplementation(query => ({
+            matches: false,
+            media: query,
+            onchange: null,
+            addEventListener: vi.fn(),
+            removeEventListener: vi.fn(),
+            dispatchEvent: vi.fn(),
+        })),
+    })
+}
+
+// add more mocks here if you need them
